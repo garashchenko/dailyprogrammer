50e51f4da7518501c7d8e4da3637d7b9aca468ba
garashchenko <m.orf676@gmail.com>
2017-07-10 18:03:05 +0500

    321 bronze: Input checks just for fun

--- 321bronze/clock.py
+++ 321bronze/clock.py
@@ -4,6 +4,12 @@ teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen'
 tens = ['oh', 'ten', 'twenty', 'thirty', 'forty', 'fifty']
 
 def talk(hours, minutes):
+    #input checks
+    if not hours.isdigit() or not minutes.isdigit():
+        return ''
+    if int(hours) > 23 or int(minutes) > 59:
+        return ''
+
     time_of_day = 'am' if int(hours) // 12 < 1 else 'pm'
     hours_parsed = clock_nums[int(hours) % 12] if (hours != '00' and hours != '12') else 'twelve'
     if minutes[0] == '1':
@@ -14,7 +20,9 @@ def talk(hours, minutes):
         minutes_parsed = ''
     return "It's %s %s %s" % (hours_parsed, minutes_parsed, time_of_day)
 
-time = input("What time it is? ")
-hours, minutes = time.split(':')
-
-print(talk(hours, minutes))
+time = input("What time is it? ")
+try:
+    hours, minutes = time.split(':')
+    print(talk(hours, minutes))
+except Exception as ex:
+    print('Check your input!')
